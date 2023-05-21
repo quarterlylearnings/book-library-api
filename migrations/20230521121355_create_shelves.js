ce89baa6d7cd1319a6771f37baa63e97c2397434
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-21 08:36:15 -0400

    Adds shelf migration instructions and routes to access table

--- migrations/20230521121355_create_shelves.js
+++ migrations/20230521121355_create_shelves.js
@@ -3,13 +3,17 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
+    return knex.schema.createTable('shelves', function(table) {
+      table.increments('id').primary();
+      table.integer('libraryId').unsigned().notNullable();
+      table.string('genre').notNullable();
+      table.timestamps(true, true);
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+      table.foreign('libraryId').references('id').inTable('libraries');
+    });
+  };
   
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTable('shelves');
+  };
+  
---
+++ routes/shelves.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db'); // Import db.js
+
+// GET all shelves
+router.get('/', function(req, res, next) {
+  knex('shelves')
+    .select('*')
+    .then(shelves => res.json(shelves))
+    .catch(err => next(err));
+});
+
+// GET a specific shelf by ID
+router.get('/:id', function(req, res, next) {
+  const { id } = req.params;
+
+  knex('shelves')
+    .select('*')
+    .where({ id })
+    .first()
+    .then(shelf => res.json(shelf))
+    .catch(err => next(err));
+});
+
+// POST a new shelf
+router.post('/', function(req, res, next) {
+  const { libraryId, genre } = req.body;
+
+  knex('shelves')
+    .insert({ libraryId, genre })
+    .returning('*')
+    .then(shelf => res.json(shelf))
+    .catch(err => next(err));
+});
+
+// PUT (update) a specific shelf by ID
+router.put('/:id', function(req, res, next) {
+  const { id } = req.params;
+  const { libraryId, genre } = req.body;
+
+  knex('shelves')
+    .update({ libraryId, genre })
+    .where({ id })
+    .returning('*')
+    .then(shelf => res.json(shelf))
+    .catch(err => next(err));
+});
+
+// DELETE a specific shelf by ID
+router.delete('/:id', function(req, res, next) {
+  const { id } = req.params;
+
+  knex('shelves')
+    .del()
+    .where({ id })
+    .returning('*')
+    .then(shelf => res.json(shelf))
+    .catch(err => next(err));
+});
+
+module.exports = router;
