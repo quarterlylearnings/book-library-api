1a1f2f399fec9caf8e6df4f0e2107712779f2f4b
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-21 17:07:29 -0400

    Adds host environment variable

--- knexfile.js
+++ knexfile.js
@@ -31,6 +31,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
+      host:     process.env.DATABASE_HOST,
       database: process.env.DATABASE_NAME,
       user:     process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD
