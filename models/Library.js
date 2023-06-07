63d4dde526601403afcde290fb1910e5f0aaad41
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-06-07 14:11:00 -0400

    Modifies Library model to contain a shelves function to be able to relate them via the models.

--- models/Library.js
+++ models/Library.js
@@ -1,8 +1,12 @@
 const bookshelf = require("../bookshelf");
+const Shelf = require("./Shelf");
 
 const Library = bookshelf.model("Library", {
     tableName: "libraries",
     idAttribute: "id",
+    shelves: function() {
+        return this.hasMany(Shelf, "libraryId")
+    }
 });
 
 module.exports = Library;
