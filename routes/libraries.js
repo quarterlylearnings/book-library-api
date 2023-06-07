613021c0c4b6f6918faa1f20419a8caedaf1f843
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-06-07 12:07:21 -0400

    Implement Bookshelf model for data access in API routes

--- routes/libraries.js
+++ routes/libraries.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db'); // Import knexfile
+const Library = require('../models/Library') // importing the Library model which already has a configuration (via Bookshelf for connecting to the database)
 
 // GET all libraries
 router.get('/', function(req, res, next) {
-  knex('libraries')
-    .select('*')
-    .then(libraries => res.json(libraries))
-    .catch(err => next(err));
-});
+
+  Library.fetchAll()
+  .then(libraries => res.json(libraries))
+  .catch(err => next(err));
+
+  // the code above should give the same response as the code below
+  // the difference is that now, we're using Bookshelf models for a
+  // less SQL-like syntax that more closely looks like how we'd describe what's taking place
+  
+  // knex('libraries')
+  //   .select('*')
+  //   .then(libraries => res.json(libraries))
+  //   .catch(err => next(err));
+  });
+
 
 // GET all shelves for a specific library by ID
 router.get('/:id/shelves', function(req, res, next) {
